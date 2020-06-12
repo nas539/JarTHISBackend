9567b5288de0a645349f299c7e5e880a7c456881
Ballred95 <blake.allred95@icloud.com>
2020-06-12 15:14:52 -0600

    added comment

--- app.py
+++ app.py
@@ -12,49 +12,21 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 heroku = Heroku(app)
 CORS(app)
+# he
 
-
-
-class AdminClass:
-    def admin_del (username) :
-        user_to_del = username
-        for (attribute in user):
-            del attribute
-            print("Username was deleted.")
-
-    def admin_del_recipe(recipe) :
-        recipe_to_del = recipe
-        del recipe_to_del
-        print ("Recipe was deleted") 
-        
-class Recipe():
-    def __init__(self, name, ID, AddedBy, Tag ):
-        self.name = name
-        self.ID = ID
-        self.AddedBy = AddedBy
-        self.Tag
 
 class Recipe(db.model):
     Name = db.Column(db.String(), primary_key=True)
     ID = db.Column(db.Integer, nullable = False
     AddedBy = db.Column(db.String(), nullable=False)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullabe=False)
-    email = db.Column(db.String, nullable=True )
-    isAdmin = db.Column(db.Boolean)
-    isLoggedIn = db.Column(db.Boolean)
-    bio = db.Column(db.String)
 
-    def __init__(self, name, email, isAdmin, isLoggedIn, bio):
+class Recipe():
+    def __init__(self, name, ID, AddedBy, Tag ):
         self.name = name
-        self.email = email
-        self.isAdmin = isAdmin
-        self.isLoggedIn = isLoggedIn
-        self.bio = bio
-
-
+        self.ID = ID
+        self.AddedBy = AddedBy
+        self.Tag
 
 
 
